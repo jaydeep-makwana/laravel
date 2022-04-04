3a7b8e2bdfcbbc09f31699ab4101240e34bf3988
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-04-04 19:29:33 +0530

    laravel

---
+++ app/Http/Controllers/interest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class interest extends Controller
+{
+    function clcInt($amount){
+        echo "your amount is $amount";
+    }
+}
---
+++ resources/views/watch.php
@@ -0,0 +1,1 @@
+<h2>Time Is Very Important </h2>
