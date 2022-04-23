46516c6abb506fcfad443f58e76293de5c7767ff
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-04-23 20:55:18 +0530

    validation added

--- app/Http/Controllers/getUserData.php
+++ app/Http/Controllers/getUserData.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Rules\email;
 use Illuminate\Http\Request;
 
 class getUserData extends Controller
@@ -16,16 +17,14 @@ class getUserData extends Controller
             "department" => "required",
             "date_of_join" => "required",
             "salary" => "required | numeric",
-            "email" => [
-                "required",
-                # Custom Validation Rule using Closures
-                function ($attribute, $value, $fail) {
-                    if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-                        $fail("invalid $attribute format");
-                    }
+            # Custom Validation Rule using Rule Objects
+            "email" => ["required", new email],
+            # Custom Validation Rule using Closures
+            "Password" => ["required", function ($attribute, $value, $fail) {
+                if (!preg_match('/[A-Z]/', $value) || !preg_match('/[a-z]/', $value) || !preg_match('/[\d]/', $value) || !preg_match('/[!@#$%&*]/', $value)) {
+                    $fail("the $attribute must contain one uppercase,lowercase,digit and following character : !@#$%&*");
                 }
-            ],
-            "Password" => "required",
+            }],
             "confirm_password" => "required | same:Password",
             "hobby" => "required"
         ]);
