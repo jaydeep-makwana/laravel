c3e84300dea25d9ef581ef5aa2b0f2a2f6108cdd
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-04-30 19:29:00 +0530

    removed some files

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,6 @@ use App\Http\Controllers\allInOne;
 use App\Http\Controllers\pincode;
 use App\Http\Controllers\event;
 use App\Http\Controllers\getUserData;
-use App\Http\Controllers\login;
-use App\Http\Controllers\welcome;
-use App\Http\Controllers\update;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +28,6 @@ Route::view("signin","main.signin");
 
 
 
-Route::view("form", "form.home");
 
 Route::get("show", [show_data::class, "show_data1"]);
 Route::get("show/{data}", [show_data::class, "show_data2"]);
@@ -41,20 +37,13 @@ Route::get("pincode", [pincode::class, "pincode"]);
 
 Route::get("event", [event::class, "event"]);
 
-Route::get("login", [login::class, "login"])->name('login');
-
-Route::get("welcome/{user}", [welcome::class, "welcome"]);
-
-Route::get("update", [update::class, "update"]);
 
 
 Route::get("clock", [allInOne::class, "clock"]);
 Route::get("todo", [allInOne::class, "todo"]);
-Route::get("user_registration", [allInOne::class, "user_registration"]);
 Route::get("reg_log", [allInOne::class, "reg_log"]);
 Route::get("js_form", [allInOne::class, "js_form"]);
 Route::get("pro_lang", [allInOne::class, "pro_lang"]);
-Route::get("dashboard", [allInOne::class, "dashboard"]);
 
 
 
@@ -64,18 +53,15 @@ Route::get("mobile/{phone}", [mobile::class, "getDetails"]);
 
 Route::get("interest/{amount}/{interest}/{duration}", [interest::class, "clcInt"]);
 
-Route::fallback(function () {
-    return view('Default');
-});
 
 Route::post("user_data", [getUserData::class, "user_data"]);
 
-# route middleware
-Route::view("home", "form.home")->middleware('route_middleware');
+
 
 Route::get("package", function () {
     echo "you are skilled employ";
 });
+
 
 # group middleware
 Route::group(["middleware" => ["group_middleware"]], function () {
@@ -83,17 +69,18 @@ Route::group(["middleware" => ["group_middleware"]], function () {
     Route::get("group_view", function () {
         echo '<h1>Welcome to web development group</h1>';
     });
-
+    
     Route::get('php', function () {
         echo "<h1>php</h1>";
     });
-
+    
     Route::get('html', function () {
         echo "<h1>html</h1>";
     });
-
+    
 });
 
-Route::get('routeMidView',function (){
- echo "This is route middleware";
+
+Route::fallback(function () {
+    return view('Default');
 });
