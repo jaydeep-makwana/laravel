55131a0258f9c910273458d5071c3b89414a0f15
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-05-06 00:20:09 +0530

    crud opration with static data

--- app/Http/Controllers/dbTest.php
+++ app/Http/Controllers/dbTest.php
@@ -8,7 +8,19 @@ use Illuminate\Support\Facades\DB;
 class dbTest extends Controller
 {
     function fetchRecord(){
-         $users = DB::select("SELECT * FROM students");
-         return view("main.admin_dashboard",["records"=>$users]);
+
+        # insert record
+        // DB::insert("INSERT INTO students (`name`,`standard`,`age`,`rollNo`) VALUES (?,?,?,?)",['Jaydeep',11,16,1]);
+
+        # update record
+        // DB::update("UPDATE students SET `name` = 'jack', `standard` = 12, `age` = 22, `rollNo` = 404 WHERE `id` = ?",[1]);
+
+        # delete record
+        // DB::delete("DELETE FROM students WHERE `id`=?",[19]);
+
+        # fetch records
+        $users = DB::select("SELECT * FROM students");
+        return view("main.admin_dashboard",["records"=>$users]);
+        
     }
 }
