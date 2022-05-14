68c9fc46c942be6e17f86bb4267506fb8e6d6196
jaydeep-makwana <jaydeepmakwana570@gmail.com>
2022-05-14 19:24:35 +0530

    checked

--- resources/views/main/signup.blade.php
+++ resources/views/main/signup.blade.php
@@ -49,15 +49,14 @@
                         <label for="">Gender <small>@error('gender'){{$message}} @enderror </small>
                             <div class="form-check">
                                 <label for="male" class="form-check-label">
-                                    <input type="radio" value="male" class="form-check-input" id="male" name="gender" 
-                                    @if(old('gender')) == 'male' checked @endif> male
+                                    <input type="radio" value="male" class="form-check-input" id="male" name="gender" @if(old('gender') == 'male' ) checked @endif> male
                                 </label>
                             </div>
                             <div class="form-check">
 
                                 <label for="female" class="form-check-label">
 
-                                    <input type="radio" value="female" class="form-check-input" id="female" name="gender" @if(old('gender') == 'female') checked @endif> female
+                                    <input type="radio" value="female" class="form-check-input" id="female" name="gender" @if(old('gender') == 'female' ) checked @endif> female
                                 </label>
                             </div>
                         </label>
@@ -67,10 +66,10 @@
                             <label for="department">Department <small>@error('department'){{$message}} @enderror </small>
                                 <select name="department" class="form-control" id="department">
                                     <option value="" selected disabled>---Choose Department</option>
-                                    <option value="R & D"  @if(old('department') == 'R & D') selected @endif>R & D</option>
-                                    <option value="Sales" @if(old('department') == 'Sales') selected @endif>Sales</option>
-                                    <option value="Marketing" @if(old('department') == 'Marketing') selected @endif>Marketing</option>
-                                    <option value="HR" @if(old('department') == 'HR') selected @endif>HR</option>
+                                    <option value="R & D" @if(old('department')=='R & D' ) selected @endif>R & D</option>
+                                    <option value="Sales" @if(old('department')=='Sales' ) selected @endif>Sales</option>
+                                    <option value="Marketing" @if(old('department')=='Marketing' ) selected @endif>Marketing</option>
+                                    <option value="HR" @if(old('department')=='HR' ) selected @endif>HR</option>
                                 </select>
                             </label>
 
@@ -134,14 +133,14 @@
                                 <input type="checkbox" class="form-check-input" name="hobby[]" id="dancing" value="dancing">
                                 <label for="dancing" class="form-check-label">Dancing</label>
                             </div>
- 
+
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" name="hobby[]" id="programming" value="programming">
                                 <label for="programming" class="form-check-label">Programming</label>
                             </div>
 
                             <div class="form-check">
-                                <input type="checkbox" class="form-check-input" name="hobby[]" id="gaming" value="gaming" >
+                                <input type="checkbox" class="form-check-input" name="hobby[]" id="gaming" value="gaming">
                                 <label for="gaming" class="form-check-label">Gaming</label>
                             </div>
 
